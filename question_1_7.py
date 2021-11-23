820713bffe709bb83e7fbca56f74c7311a207db5
Olsi Sadiku <olsi_sadiku@hotmail.com>
2021-11-22 18:16:19 -0600

    need to fix question 1.7)

--- question_1_7.py
+++ question_1_7.py
@@ -1,14 +1,34 @@
 
 def rotate_matrix(matrix):
     size = len(matrix) - 1 
+    length = len(matrix)
     start = 0
     for i in range(len(matrix)):
-        for j in range(start, len(matrix[i])):
-            temp = matrix[i][j]
-            temp2 = matrix[j][abs(i-size)]
-        
-        if(start < len(matrix)):
+        for j in range(start, length-start):
+            top = matrix[i][j]
+            print("I:", i, "J:", j)
 
+            right = matrix[j][abs(i-size-start) % length]
+            print("I:", j, "J:", abs(i-size-start) % length)
+            
+            bottom = matrix[abs(i-size-start) % length][abs(j-size-start) % length]
+            print("I:", (abs(i-size-start) % length), "J:", abs(j-size-start) % length)
+
+            left = matrix[abs(j-size-start) % length ][abs(i-size-start+size) % length]
+            print("I:", abs(j-size-start) % length, "J:", abs(i-size-start+size) % length )
+
+            matrix[j][abs(i-size-start) % length] = top
+
+            matrix[abs(i-size-start) % length][abs(j-size-start) % length] = right
+
+            matrix[abs(j-size-start) % length ][abs(i-size-start+size) % length] = bottom
+
+            matrix[i][j] = left 
+
+            print()
+            print()
+
+        if(start < length):
             start+=1
         else:
             break
@@ -18,10 +38,10 @@ def rotate_matrix(matrix):
 
 
 matrix = [
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
-    [13,14,15,16]
+    [1,1,1,1],
+    [2,2,2,2],
+    [3,3,3,3],
+    [4,4,4,4]
 ]
 
-rotate_matrix(matrix)
+print(rotate_matrix(matrix))
